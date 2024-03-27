6d24a2ccae6a23e97adb4de35fd525e13c746b7a
sourav sanyal <125809452+webdevguru@users.noreply.github.com>
2024-03-28 00:22:33 +0530

    Update homeworrk.cpp

--- two pointer/code/homeworrk.cpp
+++ two pointer/code/homeworrk.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// Q1. 
+
+// Q2. Remove Duplicates from Sorted Array.
+int removeDuplicates(vector<int> &A) {
+    int n=A.size();
+    int i=0,j=0,ans=0;
+    while(i<n){
+        while(A[i]==A[i+1] && i<n-1) i++;
+        swap(A[i++],A[j++]);
+        ans++;
+    }
+    return ans;
+}
 
